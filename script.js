95730323b536bc10ea397451346770ee58606308
RicardoAmador1 <ricardoamadorj@gmail.com>
2022-10-12 22:19:24 -0500

    Se añade formularios y class de las noticias de la pagina

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 'use sctrict';
 
 
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
 
 /**
  * add event on element
@@ -33,5 +51,3 @@
    
    addEventOnElem(navLinks, "click", closeNavbar);
 
-   
-   
